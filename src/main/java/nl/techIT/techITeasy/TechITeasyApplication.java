01b40c6a8648268f2ee77dda1888fcfe078ef310
Jasper Paul Krol <jasper.paul.krol@gmail.com>
2021-11-04 13:09:58 +0100

    first commit

---
+++ src/main/java/nl/techIT/techITeasy/TechITeasyApplication.java
@@ -0,0 +1,13 @@
+package nl.techIT.techITeasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechITeasyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechITeasyApplication.class, args);
+	}
+
+}
