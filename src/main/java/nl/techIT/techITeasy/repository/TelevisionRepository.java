daaaa947f3753c6ffcb52eebd36b7dfa55411291
Jasper Paul Krol <jasper.paul.krol@gmail.com>
2021-11-05 16:14:09 +0100

    repository start

---
+++ src/main/java/nl/techIT/techITeasy/repository/TelevisionRepository.java
@@ -0,0 +1,7 @@
+package nl.techIT.techITeasy.repository;
+
+import nl.techIT.techITeasy.model.Television;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public class TelevisionRepository extends JpaRepository<Television, Integer> {
+}
